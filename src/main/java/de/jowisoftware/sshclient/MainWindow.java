01e2e097051513074609f0c789961e719e8cebad
jochenwierum <jochen@jowisoftware.de>
2011-07-05 20:35:49 +0000

    [NF] More UI-Improvements: added view menu

--- src/main/java/de/jowisoftware/sshclient/MainWindow.java
+++ src/main/java/de/jowisoftware/sshclient/MainWindow.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -21,6 +22,7 @@ import com.jcraft.jsch.JSchException;
 
 import de.jowisoftware.sshclient.jsch.KeyAgentManager;
 import de.jowisoftware.sshclient.log.LogPanel;
+import de.jowisoftware.sshclient.ui.DnDTabbedPane;
 import de.jowisoftware.sshclient.ui.PrivateKeyTab;
 import de.jowisoftware.sshclient.ui.SSHFrame;
 
@@ -29,6 +31,8 @@ public class MainWindow extends JFrame {
     private JTabbedPane pane;
     private JSch jsch;
     private Timer timer;
+    private Component logPanel;
+    private PrivateKeyTab privateKeyTab;
 
     public MainWindow() {
         super("SSH");
@@ -83,26 +87,24 @@ public class MainWindow extends JFrame {
     private void initWindowElements() {
         final JMenuBar menu = new JMenuBar();
         final JMenu fileMenu = createFileMenu();
+        final JMenu viewMenu = createViewMenu();
         menu.add(fileMenu);
+        menu.add(viewMenu);
 
-        pane = new JTabbedPane();
+        pane = new DnDTabbedPane();
 
         setJMenuBar(menu);
         add(pane);
 
-        addLogTab();
-        addPrivateKeyTab();
+        initTabs();
 
         setSize(640, 480);
         setVisible(true);
     }
 
-    private void addPrivateKeyTab() {
-        pane.addTab("Key Agent", new PrivateKeyTab(jsch));
-    }
-
-    private void addLogTab() {
-        pane.addTab("Log", new LogPanel());
+    private void initTabs() {
+        privateKeyTab = new PrivateKeyTab(jsch);
+        logPanel = new LogPanel();
     }
 
     private void initJSch() throws JSchException {
@@ -166,25 +168,81 @@ public class MainWindow extends JFrame {
         return fileMenu;
     }
 
+
+    private JMenu createViewMenu() {
+        final JMenu viewMenu = new JMenu("View");
+        viewMenu.setMnemonic('v');
+
+        viewMenu.add(createLogVisibilityEntry());
+        viewMenu.add(createKeyAgentVisibilityEntry());
+
+        return viewMenu;
+    }
+
+
+    private JMenuItem createLogVisibilityEntry() {
+        final JCheckBoxMenuItem entry = new JCheckBoxMenuItem("Show Logs");
+        entry.setMnemonic('l');
+
+        entry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                if (entry.isSelected()) {
+                    pane.addTab("log", logPanel);
+                } else {
+                    for (int i = 0; i < pane.getTabCount(); ++i) {
+                        if (pane.getComponentAt(i) == logPanel) {
+                            pane.removeTabAt(i);
+                        }
+                    }
+                }
+            }
+        });
+
+        return entry;
+    }
+
+    private JMenuItem createKeyAgentVisibilityEntry() {
+        final JCheckBoxMenuItem entry = new JCheckBoxMenuItem("Key Agent");
+        entry.setMnemonic('a');
+
+        entry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                if (entry.isSelected()) {
+                    pane.addTab("Keys", privateKeyTab);
+                } else {
+                    for (int i = 0; i < pane.getTabCount(); ++i) {
+                        if (pane.getComponentAt(i) == privateKeyTab) {
+                            pane.removeTabAt(i);
+                        }
+                    }
+                }
+            }
+        });
+
+        return entry;
+    }
+
     private JMenuItem createConnectEntry() {
-        final JMenuItem connect = new JMenuItem("Connect...");
-        connect.addActionListener(new ActionListener() {
+        final JMenuItem entry = new JMenuItem("Connect...");
+        entry.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
                 createConnection();
             }
         });
-        return connect;
+        return entry;
     }
 
     private JMenuItem createQuitMenuEntry() {
-        final JMenuItem quit = new JMenuItem("Quit");
-        quit.addActionListener(new ActionListener() {
+        final JMenuItem entry = new JMenuItem("Quit");
+        entry.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
                 dispose();
             }
         });
-        return quit;
+        return entry;
     }
 }
