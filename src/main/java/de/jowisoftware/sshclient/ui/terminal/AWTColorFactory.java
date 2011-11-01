bf2194b65dfb5240e6ac3f1897cbf70f290a5094
jochenwierum <jochen@jowisoftware.de>
2011-11-01 17:27:21 +0000

    [NF #24] Custom colors now have default values

--- src/main/java/de/jowisoftware/sshclient/ui/terminal/AWTColorFactory.java
+++ src/main/java/de/jowisoftware/sshclient/ui/terminal/AWTColorFactory.java
@@ -4,11 +4,16 @@ import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import de.jowisoftware.sshclient.terminal.gfx.ColorFactory;
 import de.jowisoftware.sshclient.terminal.gfx.ColorName;
 import de.jowisoftware.sshclient.terminal.gfx.TerminalColor;
 
 public class AWTColorFactory implements ColorFactory {
+    private static final Logger LOGGER = Logger
+            .getLogger(AWTColorFactory.class);
+
     private class SystemColor implements TerminalColor {
         private final ColorName colorName;
         private final boolean isForeground;
@@ -69,6 +74,28 @@ public class AWTColorFactory implements ColorFactory {
     public AWTColorFactory(final AWTGfxInfo gfxInfo) {
         this.gfxInfo = gfxInfo;
         this.customColors = new HashMap<Integer, Color>();
+        initDefaultColors();
+
+    }
+
+    private void initDefaultColors() {
+        for (int red = 0; red < 6; red++) {
+            for (int green = 0; green < 6; green++) {
+                for (int blue = 0; blue < 6; blue++) {
+                    final int colorKey = (red * 36) + (green * 6) + blue + 16;
+                    customColors.put(colorKey, new Color(
+                            (red > 0 ? (red * 40 + 55) : 0),
+                            (green > 0 ? (green * 40 + 55) : 0),
+                            (blue > 0 ? (blue * 40 + 55) : 0)));
+                }
+            }
+        }
+
+        final int NUMBER_OF_COLORS = 232;
+        for (int i = 0; i < 24; i++) {
+            final int rgb = (i * 10) + 8;
+            customColors.put(NUMBER_OF_COLORS + i, new Color(rgb, rgb, rgb));
+        }
     }
 
     @Override
@@ -100,6 +127,8 @@ public class AWTColorFactory implements ColorFactory {
     @Override
     public void updateCustomColor(final int colorCode,
             final int red, final int green, final int blue) {
+        LOGGER.debug("Setting new cucstom color " + colorCode + " to "
+                + red + "/" + green + "/" + blue);
         customColors.put(colorCode, new Color(red, green, blue));
     }
 
