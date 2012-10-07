4d8372d3df3fd9f4fe40103280ff9ca95cefc35a
Jochen Wierum <jochen@jowisoftware.de>
2012-10-07 12:18:31 +0200

    [DW] replaced manual centering on parent through the buildin method

--- src/main/java/de/jowisoftware/sshclient/ui/about/AboutDialog.java
+++ src/main/java/de/jowisoftware/sshclient/ui/about/AboutDialog.java
@@ -8,7 +8,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -52,15 +51,7 @@ public class AboutDialog extends JDialog {
         setResizable(false);
         setSize(new Dimension(660, 500));
         setModal(true);
-        centerOnParent(parent);
-    }
-
-    private void centerOnParent(final JFrame parent) {
-        final Rectangle bounds = parent.getBounds();
-        final Dimension size = getSize();
-
-        setLocation(bounds.x + (bounds.width - size.width) / 2 ,
-                bounds.y + (bounds.height - size.height) / 2);
+        setLocationRelativeTo(parent);
     }
 
     private void createContents() {
